6823c659e720d720988d941a762d0420d0fea52c
QuynCM <quycm@edsolabs.com>
2023-04-27 11:11:23 +0700

    update code

--- app/page.tsx
+++ app/page.tsx
@@ -11,24 +11,44 @@ export default function Home() {
   function handleAddToCart(x: number, y: number, image: string) {
     setCartCount(cartCount + 1);
 
+
     const cart: any = document.querySelector('.cart-icon') || {};
     const productImage = document.createElement("div");
 
     // Create a clone of the product image
-    const clonedProductImage: any = productImage?.cloneNode();
-    clonedProductImage.style.position = 'absolute';
-    clonedProductImage.style.width = `100px`;
-    clonedProductImage.style.height = "100px";
-    clonedProductImage.style.backgroundImage = `url(${image})`
+    const xAxis: any = productImage?.cloneNode();
+    xAxis.style.position = 'absolute';
+    xAxis.style.width = `100px`;
+    xAxis.style.height = "100px";
+    xAxis.style.background = `url("${image}")`
+    xAxis.style.backgroundSize = `cover`
+
+    const yAxis = document.createElement("div")
+    yAxis.appendChild(xAxis)
+    yAxis.style.position = 'absolute';
+    yAxis.style.width = `100px`;
+    yAxis.style.height = "100px";
 
     // Add the cloned product image to the DOM
-    document.body.appendChild(clonedProductImage);
+    document.body.appendChild(yAxis);
 
     // Animate the cloned product image to the cart
-    const animation = clonedProductImage.animate(
+    const animationXAxis = xAxis.animate(
       [
-        { left: `${x}px`, top: `${y}px`, transform: "scale(1.0)", opacity: 0.6 },
-        { left: `${cart.offsetLeft + 120}px`, top: `${cart.offsetTop}px`, transform: "scale(0.7)", opacity: 0 },
+        { left: `${x - cart.offsetLeft - 120}px`, top: `${0}px` },
+        { left: `${0}px`, top: `${0}px` },
+      ],
+      {
+        duration: 1200,
+        easing: 'linear',
+        fill: 'forwards',
+      }
+    );
+
+    const animationYAxis = yAxis.animate(
+      [
+        { left: `${cart.offsetLeft + 120}px`, top: `${y}px`, transform: "scale(1.0)", opacity: .8 },
+        { left: `${cart.offsetLeft + 120}px`, top: `${cart.offsetTop}px`, transform: "scale(0.6)", opacity: 0.1 },
       ],
       {
         duration: 1200,
@@ -38,8 +58,12 @@ export default function Home() {
     );
 
     // Remove the cloned product image from the DOM after the animation is done
-    animation.onfinish = () => {
-      clonedProductImage.remove();
+    animationXAxis.onfinish = () => {
+      xAxis.remove();
+    };
+
+    animationYAxis.onfinish = () => {
+      yAxis.remove();
     };
   }
 
@@ -52,25 +76,25 @@ export default function Home() {
         <Product
           name="Product 1"
           price="19.99"
-          image="https://via.placeholder.com/350x150"
+          image="https://cdn.shopify.com/s/files/1/0024/9803/5810/products/596667-Product-0-I-637982213840849162.jpg?v=1662700221"
           onAddToCart={handleAddToCart}
         />
         <Product
           name="Product 2"
           price="24.99"
-          image="https://via.placeholder.com/350x150"
+          image="https://cdn.shopify.com/s/files/1/0024/9803/5810/products/596667-Product-0-I-637982213840849162.jpg?v=1662700221"
           onAddToCart={handleAddToCart}
         />
         <Product
           name="Product 3"
           price="14.99"
-          image="https://via.placeholder.com/350x150"
+          image="https://cdn.shopify.com/s/files/1/0024/9803/5810/products/596667-Product-0-I-637982213840849162.jpg?v=1662700221"
           onAddToCart={handleAddToCart}
         />
         <Product
           name="Product 4"
           price="29.99"
-          image="https://via.placeholder.com/350x150"
+          image="https://cdn.shopify.com/s/files/1/0024/9803/5810/products/596667-Product-0-I-637982213840849162.jpg?v=1662700221"
           onAddToCart={handleAddToCart}
         />
       </div>
